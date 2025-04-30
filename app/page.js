2f3a6f8f52a7dc246000a839b4bb472fbb650d2c
Kanan Badirli <kanan.badirli@fpay.az>
2025-04-30 11:08:54 +0400

    .

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ import Footer from "./Footer";
 export default function Home() {
   return (
     <div>
-      <Navbar/>
+      {/* <Navbar/> */}
       <Hero/>
       <PaketListesi />
       <Avantajlar/>
