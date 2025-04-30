caba78527f65d8695f8f20bb5aee762134ba881e
Kanan Badirli <kanan.badirli@fpay.az>
2025-04-30 20:54:54 +0400

    font changed

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,15 @@
 const config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        gilroy: ["Gilroy", "sans-serif"],
+      },
+    },
+  },
   plugins: ["@tailwindcss/postcss"],
 };
 
